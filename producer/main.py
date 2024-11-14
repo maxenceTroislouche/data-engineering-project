b58883075ea6c83f2468097fbe35cf3585f6c13b
maxenceTroislouche <maxence62700@outlook.fr>
2024-11-14 11:18:02 +0100

    Updated: docker-compose + producer

--- producer/main.py
+++ producer/main.py
@@ -16,22 +16,9 @@ def delivery_report(err, msg):
     else:
         print(f"Message delivered to {msg.topic} [{msg.partition}] at offset {msg.offset}")
 
+
 pacemaker = Pacemaker(id=1)
 measure = generate_measure_for_pacemaker(pacemaker)
 
 producer.send_message('pacemaker-measures', get_json_string_from_pacemaker_measure(measure), delivery_report)
-# N = 5
-# topic = 'my_topic'  # Nom du topic Kafka
-
-# Envoi des messages toutes les N secondes
-# try:
-#     while True:
-#         producer.send_message(topic, data, delivery_report)
-#         print(f"Sent data: {json.dumps(data)}")
-#         time.sleep(N)  # Attente de N secondes avant d'envoyer à nouveau
-# except KeyboardInterrupt:
-#     print("Process interrupted by user")
-
-
-# Assurez-vous que tous les messages sont envoyés avant de quitter
 producer.flush()
